b510be4bbbc4565ac5dfb136c8b72736dbee8286
Rhawan81 <rhawanjs@gmail.com>
2025-07-25 22:02:28 -0300

    lacos_de_repeticao

---
+++ task1.py
@@ -0,0 +1,4 @@
+# Contagem de 1 a 10
+##Imprima os números de 1 a 10 usando um laço de repetição.
+for conte in range(0,11): # -> Aqui o contador irá apresentar 10 numeros exibindo somente até o 10: 
+    print(conte)
